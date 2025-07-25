340cab997bce7eb33d0c2e8b9dd53870fef6bde2
Sergio Bernardi <sergiorbj93@gmail.com>
2025-07-25 00:22:19 -0300

    feat: record room audio api request

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
+
+type RecordRoomParams = {
+  id: string;
+};
 
 const isRecordingSupported =
   !!navigator.mediaDevices &&
@@ -7,10 +12,33 @@ const isRecordingSupported =
   typeof window.MediaRecorder === 'function';
 
 export function RecordRoomAudio() {
+  const params = useParams<RecordRoomParams>();
   const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
 
-  async function stopRecording() {
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+    formData.append('file', audio, 'audio.webm');
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.id}/audio`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    const result = response.json();
+
+    return result;
+  }
+
+  function stopRecording() {
     setIsRecording(false);
+
+    if (recorder?.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop();
+    }
   }
 
   async function startRecording() {
@@ -29,27 +57,35 @@ export function RecordRoomAudio() {
       },
     });
 
-    const recorder = new MediaRecorder(audio, {
+    recorder.current = new MediaRecorder(audio, {
       mimeType: 'audio/webm',
       audioBitsPerSecond: 64_000,
     });
 
-    recorder.ondataavailable = (event) => {
+    recorder.current.ondataavailable = (event) => {
       if (event.data.size > 0) {
         // biome-ignore lint/suspicious/noConsole: log for debugging
         console.log(event.data);
+
+        uploadAudio(event.data);
       }
     };
 
-    recorder.onstart = () => {
+    recorder.current.onstart = () => {
       // biome-ignore lint/suspicious/noConsole: log for debugging
       console.log('Record Started');
     };
 
-    recorder.onstop = () => {
+    recorder.current.onstop = () => {
       // biome-ignore lint/suspicious/noConsole: log for debugging
       console.log('Record paused/finished');
     };
+
+    recorder.current.start();
+  }
+
+  if (!params.id) {
+    return <Navigate replace to="/" />;
   }
 
   return (
